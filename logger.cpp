32333b9e490dc158b80a726134c9ac37d6de3045
giper5ik <radovojila@gmail.com>
2025-02-19 17:04:42 +0300

    Add files via upload

---
+++ logger.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+// TODO to connect logger
+// 1.c/c++ => general => additional include directories => spdlog incl
+// 2. Перейдите в раздел Configuration Properties → C/C++ → Command Line.
+//В поле Additional Options добавьте флаг / utf - 8.
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+int main()
+{
+   AllocConsole();
+   auto console = spdlog::stdout_color_mt("console");
+   console->error("Kazahstan threats of bombing");
+}
+
